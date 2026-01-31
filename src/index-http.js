8eea4ab6713105ccbe1e798a36cbf6e63bf45ac2
Arjan van der Meer <github.com@arjan.vdmholding.com>
2026-01-31 20:17:08 +0000

    fixes to the URI to support CSP

--- src/index-http.js
+++ src/index-http.js
@@ -19,7 +19,7 @@ import { CallToolRequestSchema, ListToolsRequestSchema, ListResourcesRequestSche
 import { TravelDatabase } from './database.js';
 import * as telemetry from './telemetry.js';
 import { render } from './templates/index.js';
-import { toolsConfig, resourcesConfig, executeToolHandler, handleReadResource, renderPOIPreview } from './tools-config.js';
+import { toolsConfig, getResourcesConfig, executeToolHandler, handleReadResource, renderPOIPreview } from './tools-config.js';
 import { versionInfo, getVersionString } from './version.js';
 import http from 'http';
 import crypto from 'crypto';
@@ -75,7 +75,18 @@ function createMCPServer() {
 
   // List available resources (MCP Apps)
   server.setRequestHandler(ListResourcesRequestSchema, async () => {
-    return resourcesConfig;
+    // Get widget domain from database config
+    const serverBaseUrl = await db.getConfig('server_base_url');
+    let widgetDomain = 'localhost';
+    if (serverBaseUrl) {
+      try {
+        widgetDomain = new URL(serverBaseUrl).hostname;
+      } catch {
+        // If URL parsing fails, use the raw value
+        widgetDomain = serverBaseUrl.replace(/^https?:\/\//, '').replace(/\/$/, '');
+      }
+    }
+    return getResourcesConfig(widgetDomain);
   });
 
   // Read resource content (MCP Apps)
